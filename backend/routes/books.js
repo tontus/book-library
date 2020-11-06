72d4464985397fb758f7b590d8f79cdfa68bc417
tontus <tonmoyshaleh@gmail.com>
2020-11-06 23:12:41 +0600

    delete books end point added

--- backend/routes/books.js
+++ backend/routes/books.js
@@ -1,19 +1,19 @@
 const router = require('express').Router();
 let Book = require('../models/book.model');
 
-router.route('/').get((req,res)=>{
+router.route('/').get((req, res) => {
     Book.find()
-    .then(books=> res.json(books))
-    .catch(err => res.status(400).json('Error: '+ err));
+        .then(books => res.json(books))
+        .catch(err => res.status(400).json('Error: ' + err));
 })
 
-router.route('/add').post((req,res)=>{
+router.route('/add').post((req, res) => {
     const name = req.body.name;
     const length = Number(req.body.length);
     const writer = req.body.writer;
     const description = req.body.description;
     const link = req.body.link;
-    
+
 
     const newBook = new Book({
         name,
@@ -23,8 +23,21 @@ router.route('/add').post((req,res)=>{
         link,
     });
     newBook.save()
-    .then(()=> res.json('Book added'))
-    .catch(err => res.status(400).json('Error: '+ err));
+        .then(() => res.json('Book added'))
+        .catch(err => res.status(400).json('Error: ' + err));
 })
+
+router.route('/:id').get((req, res) => {
+    Book.findById(req.params.id)
+        .then(book => res.json(book))
+        .catch(err => res.status(400).json('Error: ' + err));
+})
+
+router.route('/:id').delete((req, res) => {
+    Book.findByIdAndDelete(req.params.id)
+        .then(() => res.json('book deleted'))
+        .catch(err => res.status(400).json('Error: ' + err));
+})
+
 
 module.exports = router; 
